8dfb5995be6b688049a070cffb3378eb4c65891b
pauljohanneskraft <pauljohanneskraft@168.198.eduroam.dynamic.rbg.tum.de>
2016-04-21 08:38:33 +0200

    initial upload

---
+++ Patterns/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Patterns
+//
+//  Created by Paul Kraft on 21.04.16.
+//  Copyright © 2016 pauljohanneskraft. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
