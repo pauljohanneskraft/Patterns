8fcaec86321405e51237a98010ac9bef1c498f90
pauljohanneskraft <pauljohanneskraft@Pauls-MacBook.local>
2016-04-21 14:43:32 +0200

    +Composite

---
+++ Patterns/Composite.swift
@@ -0,0 +1,17 @@
+//
+//  Composite.swift
+//  Patterns
+//
+//  Created by Paul Kraft on 21.04.16.
+//  Copyright © 2016 pauljohanneskraft. All rights reserved.
+//
+
+import Cocoa
+
+class Component {}
+
+class Composite : Component {
+    private var children : [Component] = []
+}
+
+class Leaf : Component {}
