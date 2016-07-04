16fcc65b50b63c5c68fbbc0bd09c6714f77a0128
Paul <pauljohanneskraft@users.noreply.github.com>
2016-07-04 02:43:01 +0200

    Update Composite.swift

--- Patterns/structural/Composite.swift
+++ Patterns/structural/Composite.swift
@@ -10,6 +10,14 @@ private struct Leaf : Component {}
 // examples:
 // 1 - Trees
 
+protocol TreeComponent {}
+
+struct TreeNode : TreeComponent {
+    var right : TreeComponent
+    var left  : TreeComponent
+}
+
+struct TreeLeaf : TreeComponent {}
 
 
 // 2 - GUI-Elements
