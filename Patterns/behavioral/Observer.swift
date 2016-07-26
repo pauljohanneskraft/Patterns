755fa35bccd8a1be49222444b13ad3ee0e16141e
Paul <pauljohanneskraft@users.noreply.github.com>
2016-07-26 23:30:22 +0200

    Observer pattern incl example rewritten from scratch
    example stayed the same in principle but a lot less code is needed, lot
    more obvious now, I think

--- Patterns/behavioral/Observer.swift
+++ Patterns/behavioral/Observer.swift
@@ -1,79 +1,95 @@
 
-private protocol _Subject {
-    var observers : [ObserverProtocol] { get set }
-}
-
-private protocol Subject : _Subject {
+// abstraction of Object having some kind of state that can be read and written to
+private protocol StateObject {
+    associatedtype State
     var state : State { get set }
 }
-extension Subject {
-    mutating func update() {
+
+// Observer protocol having a state, as well as an update-function
+private protocol Observer : StateObject {
+    mutating func updateOccurred()
+    // you can customize yourself what you want to do with the changed state
+}
+
+extension Observer {
+    mutating func update < S : Subject where S.State == Self.State > (sub: S) {
+        self.state = sub.state
+        updateOccurred()
+    }
+}
+
+// Subject protocol having a state, as well as some Observers in an Array
+private protocol Subject : StateObject {
+    associatedtype O : Observer
+    var observers : [O] { get set }
+}
+
+// when the State-Types are the same, observers can subscribe to subject, as well as they can be notified
+extension Subject where O.State == Self.State {
+    mutating func subscribe(obs: O) {
+        observers.append(obs)
+    }
+    
+    mutating func notify() {
         for i in observers.indices {
-            observers[i].update(self)
+            observers[i].update(sub: self)
         }
     }
 }
 
 
-private protocol _ObserverProtocol {
-    mutating func update(_ subject: Subject)
-}
 
-private protocol State {}
 
-private protocol ObserverProtocol : _ObserverProtocol {
-    var observedState : State { get set }
-}
+// Example 1: Reading from the console and then writing the input
 
-// example 1
-
-import Cocoa
-
-private enum ReadingState : State {
+private enum ReadingState {
     case read(String)
+    case initial
 }
 
-let stdin = NSFileHandle.standardInput()
+private struct Reader : Subject {
+    ///
+    /// notifies all observers as soon as the state is changed
+    ///
+    /// --> don't use when you have intermediate states used in a function
+    /// alternative: switch to using notify() in the methods itself or change didSet to sth like
+    ///
+    /// var state : ReadingState = .initial { didSet { if state != .initial { notify() } }
+    ///
+    var state : ReadingState = .initial { didSet { notify() } }
+    
+    
+    ///
+    /// objects observing state of the Reader
+    ///
+    private(set) var observers: [ReadingObserver] = []
+}
 
-private struct EventSource : Subject {
-    var observers = [ObserverProtocol]()
-    var state : State = ReadingState.read("")
-    mutating func run() {
-        while true {
-            let response = String(NSString(data: stdin.availableData, encoding: NSUTF8StringEncoding)!)
-            state = ReadingState.read(response)
-            update()
-            if response.lowercased().hasPrefix("stop") {
-                print("subject will stop.")
-                return
-            }
-        }
+private struct ReadingObserver : Observer {
+    private(set) var state: ReadingState = .initial
+    
+    mutating func updateOccurred() {
+        print(state)
     }
 }
 
-private struct Observer : ObserverProtocol {
-    var observedState : State
-    init(_ initialState: State) {
-        self.observedState = initialState
+public func testReader() {
+    var reader = Reader()
+    
+    reader.subscribe(obs: ReadingObserver())
+    
+    var string = ""
+    
+    while string != "stop" {
+        print("Please insert some input. To stop please enter \"stop\"")
+        string = readLine(strippingNewline: true)!
+        reader.state = .read(string)
     }
-    mutating func update(_ subject: Subject) {
-        observedState = subject.state
-        print(subject.state)
-    }
+    
+    print("done")
 }
 
-private func observerDemo1() {
-    print("Enter text > ")
-    var source = EventSource()
-    let obs = Observer(ReadingState.read(""))
-    source.observers.append(obs)
-    source.run()
-}
 
-func observerDemo() {
-    print("Demo 1: Input")
-    observerDemo1()
-}
 
 
 
